59bb9441b3338a87779dd8e52381d412a0715ae8
Pavel <Pawelv9@gmail.com>
2018-11-26 01:40:51 +0300

    lesson-1

--- exercises/connect.js
+++ exercises/connect.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-let url = "mongodb://localhost:27017/intro-to-mongodb";
 
-const connect = (url) =>
-  mongoose.connect(url);
-mongoose.connection
-  .once('open', () => { 
-    console.log('CONNECTED') })
-  .on('error', (error) => {
-    console.warn('Warning', error);
-  })
+const connect = url =>
+  mongoose.connect(url,
+    {useNewUrlParser: true});
+
+// mongoose.connection
+//   .once('open', () => { 
+//     console.log('CONNECTED') })
+//   .on('error', (error) => {
+//     console.warn('Warning', error);
+//   })
 
 module.exports = connect()
